3779b42d08daf196d40306780b92f8293ca56b94
Renz Lacerna <renzlacerna13@gmail.com>
2023-11-20 00:04:32 -0500

    almost done

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const session = require("express-session");
+const methodOverride = require("method-override");
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-});
+const authRoutes = require("./controllers/authController");
+const placeRoutes = require("./controllers/placeController");
+const reservationRoutes = require("./controllers/reservationController");
+const reviewRoutes = require("./controllers/reviewController");
 
-mongoose.connection
-    .on('open', () => console.log('Ready to go!'))
-    .on('close', () => console.log('Connection closed.'))
-    .on('error', (e) => console.log("error:", e))
-module.exports = mongoose
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(session({ secret: "where2", cookie: { maxAge: 6000000 } }));
+
+app.use(express.json());
+
+app.use("/user", authRoutes);
+app.use("/place", placeRoutes);
+app.use("/reservation", reservationRoutes);
+app.use("/review", reviewRoutes);
+
+
+
+app.listen(PORT, () => console.log("Where2 is listening on port:", PORT));
